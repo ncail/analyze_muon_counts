f452abe90b6bbebd7488e97577f3dd8beb6918aa
ncail <ncail@islander.tamucc.edu>
2025-02-25 12:29:06 -0600

    trying again to disable printing to console.

--- pythonSerialLogger/serialLogger.py
+++ pythonSerialLogger/serialLogger.py
@@ -43,11 +43,11 @@ port = args.port if args.port else 'COM3'
 baudrate = 9600
 
 # Configure logger. Write to new file when log file gets to 5 MB, save 3 backups.
+logging.getLogger().handlers.clear()  # Dont write to console.
 log_handler = RotatingFileHandler("app.log", maxBytes=5 * 1024 * 1024, backupCount=3)
 log_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-logger.handlers.clear()  # Dont write to console.
 logger.addHandler(log_handler)
 logger.info("Logging started.")
 
