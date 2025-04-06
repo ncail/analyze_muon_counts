96416e5a22a50fd91a6ff61aafb5a45f644612ea
ncail <ncail@islander.tamucc.edu>
2025-04-05 22:49:04 -0500

    keep datetime as utc.

--- preprocess_omni_data.py
+++ preprocess_omni_data.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 ''' ************************************************ Config ************************************************ '''
 data_lst = ('C:\\data\\project_of_excellence\\muon_project_programs\\data_files\\omni_data'
-            '/omni2_Iz3WfPPexI.lst')
-output_path = f'preprocessed_data/omni_data/preprocessed_hourly_omni2_Iz3WfPPexI.csv'
+            '/omni2_daily_1xLBebsoh3.lst')
+output_path = f'preprocessed_data/omni_data/preprocessed_omni2_daily_1xLBebsoh3.csv'
 
 
 ''' ********************************************* Processing *********************************************** '''
@@ -20,12 +20,12 @@ df['datetime'] = pd.to_datetime(df['year'].astype(str) + df['doy'].astype(str).s
 # Add hour as a timedelta.
 df['datetime'] = df['datetime'] + pd.to_timedelta(df['hour'], unit='h')
 
-# Convert to local time.
-df['datetime_CT'] = df['datetime'].dt.tz_convert('America/Chicago')
+# Set as UTC.
+df['datetime'] = pd.to_datetime(df['datetime'], utc=True)
 
 # Edit columns.
-df.drop(columns=['year', 'doy', 'hour', 'datetime'])
-reorder_cols = ['datetime_CT', 'Scalar_B_nT', 'SW_Plasma_Speed_kmps', 'R_Sunspot_No', 'f10_7_index']
+df.drop(columns=['year', 'doy', 'hour'])
+reorder_cols = ['datetime', 'Scalar_B_nT', 'SW_Plasma_Speed_kmps', 'R_Sunspot_No', 'f10_7_index']
 df = df[reorder_cols]
 
 # Save to CSV.
