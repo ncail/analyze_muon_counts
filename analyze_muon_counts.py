8200b6c81e23837ae68f0793d5cd33fa002de5c0
ncail <ncail@islander.tamucc.edu>
2025-04-26 15:18:05 -0500

    added configs for rolling rate plotting, and allow config fo plotting wavelet spectrogram in units of freq or scales, added plotting of lambda derivative in rolling rate mode.

--- analyze_muon_counts.py
+++ analyze_muon_counts.py
@@ -1,5 +1,5 @@
 # Imports.
-import datetime
+from datetime import datetime
 import json
 import pandas as pd
 import numpy as np
@@ -12,12 +12,23 @@ import seaborn as sns
 
 
 ''' ***************************************** CONFIG ******************************************** '''
-# Config mode (of plotting results): 0 - raw data, 1 - fft, 2 - wavelet
+# Config mode (of plotting results): 0 - raw data, 1 - fft, 2 - wavelet,
+# 3 - rolling rate, 4 - histogram.
 mode = 2
+
+# Plot the wavelet transform in units of frequency. Else units of scale.
+mode_2_freq = False
+
+# x-axis limits for rolling rate mode (zoom in on a region corresponding to a hotspot on the wavelet transform
+# spectrogram).
+x_limits = 0  # [datetime(YYYY, MM, DD), datetime(YYYY, MM, DD)]
 
 # Get input and output paths.
 data_filepath = 'preprocessed_data/muon_data/preprocessed_1H-intervals_20250227_132422.csv'
 output_path = 'results/spectral_analysis_plots'
+
+# Text for labelling the axes of figures.
+data_label = 'Counts (per hour)'
 
 # Config signal smoothing.
 gaussian_smoothing_sigma = 0
@@ -28,6 +39,12 @@ low_pass_filter = 1/(6*3600)  # Cutoff frequency.
 # Wavelet. Bandwidth-Center frequency.
 wavelet = 'cmor1.5-1.0'
 
+# Indices of top 5 FFT frequencies to plot on wavelet spectogram. Choose from FFT results.
+fft_lines = [2]
+
+# Window (number of points) for mode 3 rolling average.
+window = 12*7
+
 
 # Returns endpoints on a log progression. For the widths of the wavelet transform.
 def get_scales(data):
@@ -36,13 +53,10 @@ def get_scales(data):
 
 ''' *************************************** PROCESSING ****************************************** '''
 # Get timestamp for filenames.
-current_timestamp = datetime.datetime.now().strftime('%m%d%Y_%H%M%S')
+current_timestamp = datetime.now().strftime('%m%d%Y_%H%M%S')
 
 # Read file into dataframe. File should be muon detector CSV file.
 df = pd.read_csv(data_filepath, parse_dates=[0])
-
-# Delete invalid rows, convert data to pandas standards:
-# timestamp column to datetime, and count column to numeric.
 column_names = df.columns
 timestamp_col = column_names[0]
 count_col = column_names[1]
@@ -84,13 +98,13 @@ magnitudes_detrended = np.abs(fft_values_detrended)
 # Convert freqs from Hz to cycles per hour.
 frequencies_per_hour = frequencies_domain * 3600
 
-# Get indices of top 3 peaks/strongest frequency components.
-top5_indices = np.argsort(magnitudes_detrended)[-5:][::-1]  # Top 3 in descending order.
+# Get indices of top 5 strongest frequency components.
+top5_indices = np.argsort(magnitudes_detrended)[-5:][::-1]  # Top 5 in descending order.
 
 # Get corresponding frequencies and magnitudes.
 top5_freqs = frequencies_domain[top5_indices]
 top5_mags = magnitudes_detrended[top5_indices]
-max_mag_freq_in_hours = 1 / frequencies_per_hour
+freq_in_hours = 1 / frequencies_per_hour
 
 # Wavelet analysis.
 scales = get_scales(event_counts_detrended)
@@ -115,9 +129,10 @@ if mode == 0:
     plt.plot(time_series, event_counts_raw, label="Data")
     plt.plot(time_series, np.full(len(time_series), background_level), color="red", label="Average")
     plt.title("Muon Counts Time Series", fontsize=20)
-    plt.xlabel("Time", fontsize=15)
+    #plt.xlabel("Time", fontsize=15)
     plt.xticks(rotation=45, fontsize=15)
-    plt.ylabel(f"Counts (per hour)", fontsize=15)
+    plt.ylabel(data_label, fontsize=15)
+    plt.yticks(fontsize=15)
     plt.legend(fontsize=15)
     # plt.grid(True)
 
@@ -127,6 +142,7 @@ if mode == 0:
     plt.savefig(f'{output_path}/{current_timestamp}_raw_data_plot.png', bbox_inches='tight')
 
     plt.show()
+    plt.close()
 # Plot raw data end.
 
 
@@ -145,7 +161,7 @@ if mode == 1:
         # Legend entries.
         dummy_line = mlines.Line2D([], [], color='m', linestyle='-')
         handles.append(dummy_line)
-        legend_labels.append(f'Peak {ind + 1}: {max_mag_freq_in_hours[top5_indices[ind]]:.2f}-hour frequency')
+        legend_labels.append(f'Peak {ind + 1}: {freq_in_hours[top5_indices[ind]]:.2f}-hour period')
     # End for.
 
     legend = plt.legend(handles=handles, labels=legend_labels, loc='best', fontsize=15)
@@ -164,6 +180,7 @@ if mode == 1:
     plt.savefig(f'{output_path}/{current_timestamp}_fft_plot.png', bbox_inches='tight')
 
     plt.show()
+    plt.close()
 # Plot ffts end.
 
 
@@ -180,52 +197,126 @@ if mode == 2:
     coi_end = time_series[-1] - pd.to_timedelta(coi, unit='h')  # End of COI at each scale.
 
     fig, ax = plt.subplots(figsize=(10, 6))
-    pcm = ax.pcolormesh(time_series, frequencies_wt, np.abs(coeffs))
-    ax.set_xlabel("Date Time", fontsize=15)
+    if mode_2_freq:
+        pcm = ax.pcolormesh(time_series, frequencies_wt, np.abs(coeffs))
+        ax.set_yscale("log")
+        ax.set_ylabel("Frequency (Hz)", fontsize=15)
+    else:
+        pcm = ax.pcolormesh(time_series, scales, np.abs(coeffs))
+        plt.ylim(top=max(scales))
+        ax.set_ylabel("Scales (hours)", fontsize=15)
+
     plt.xticks(rotation=45, ha='right', fontsize=12)
     plt.yticks(fontsize=12)
-    ax.set_yscale("log")
-    ax.set_ylabel("Frequency (Hz)", fontsize=15)
     ax.set_title("Muon Count Wavelet Transform", fontsize=25)
     fig.colorbar(pcm, ax=ax)
 
-    # legend_labels = []
-    # handles = []
-    # for ind, peak in enumerate(top5_freqs):
-    #     plt.axhline(y=peak, color='m', linestyle='--')
+    # Plot the strongest freq/period lines (chosen from FFT). Aggregate all the line labels in the loop,
+    # then plot final line with the full label after the loop.
+    label = 'From FFT: '
+    first = True
+    for ind in fft_lines:
+        if first:
+            label += f'{freq_in_hours[top5_indices[ind]]:.2f}-hour period'
+            first = False
+        else:
+            label += f'\n{freq_in_hours[top5_indices[ind]]:.2f}-hour period'
+            if mode_2_freq:  # Plot the line in units of frequency.
+                plt.axhline(y=top5_freqs[ind], color='m', linestyle='--')
+            else:  # Plot line in units of scale.
+                plt.axhline(y=freq_in_hours[top5_indices[ind]], color='m', linestyle='--')
+    # End for.
 
-    #     # Legend entries.
-    #     dummy_line = mlines.Line2D([], [], color='m', linestyle='-')
-    #     handles.append(dummy_line)
-    #     legend_labels.append(f'Peak {ind + 1} from FFT: {max_mag_freq_in_hours[top5_indices[ind]]:.2f}-hour
-    #     frequency')
-    # # End for.
-    # legend_labels.append('COI')
-    # legend = plt.legend(handles=handles, labels=legend_labels, loc='best', fontsize=12)
-    # legend.get_frame().set_edgecolor('black')
-
-    # Plot ~24-hr frequency.
-    plt.axhline(y=top5_freqs[2], color='m', linestyle='--',
-                label=f'From FFT: '
-                      f'{max_mag_freq_in_hours[top5_indices[2]]:.2f}-hour frequency')
-                      # f'\n{max_mag_freq_in_hours[top5_indices[3]]:.2f}-hour frequency')
-    # plt.axhline(y=top5_freqs[3], color='m', linestyle='--')
+    # Plot first fft line with full label.
+    if mode_2_freq:  # Plot the line in units of frequency.
+        plt.axhline(y=top5_freqs[0], color='m', linestyle='--', label=label)
+    else:  # Plot line in units of scale.
+        plt.axhline(y=freq_in_hours[top5_indices[fft_lines[0]]], color='m', linestyle='--', label=label)
 
     # Plot COI.
     valid = coi_start < coi_end
-    plt.fill_betweenx(frequencies_wt[valid], time_series[0], coi_start[valid],
-                      color='white', alpha=0.3, hatch='//')
-    plt.fill_betweenx(frequencies_wt[valid], coi_end[valid], time_series[-1],
-                      color='white', alpha=0.3, hatch='//')
-    # plt.plot(coi_start, frequencies_wt, 'w--', label='COI')
-    # plt.plot(coi_end, frequencies_wt, 'w--')
+    if mode_2_freq:
+        plt.fill_betweenx(frequencies_wt[valid], time_series[0], coi_start[valid],
+                          color='white', alpha=0.3, hatch='//')
+        plt.fill_betweenx(frequencies_wt[valid], coi_end[valid], time_series[-1],
+                          color='white', alpha=0.3, hatch='//')
+    else:
+        plt.fill_betweenx(scales[valid], time_series[0], coi_start[valid],
+                          color='white', alpha=0.3, hatch='//')
+        plt.fill_betweenx(scales[valid], coi_end[valid], time_series[-1],
+                          color='white', alpha=0.3, hatch='//')
 
     plt.legend(fontsize=15)
     plt.savefig(f'{output_path}/{current_timestamp}_wavelet_plot.png', bbox_inches='tight')
 
     plt.show()
+    plt.close()
 # Plot wavelet transform end.
 
+
+# Plot rolling rate.
+if mode == 3:
+    df['lambda_est'] = df[count_col].rolling(window=window, center=True, min_periods=1).mean()
+    df['lambda_derivative'] = df['lambda_est'].diff()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df[timestamp_col], df[count_col], label=data_label, alpha=0.4)
+    plt.plot(df[timestamp_col], df['lambda_est'], label='Expected rate λ(t)', color='red')
+    plt.plot(df[timestamp_col], df['lambda_derivative'], label='dλ/dt', color='green')
+
+    if x_limits:
+        plt.xlim(left=x_limits[0], right=x_limits[1])
+
+    plt.title('Time-varying Muon Count Rate', fontsize=20)
+    plt.xticks(rotation=45, ha='right', fontsize=15)
+    plt.ylabel(data_label, fontsize=15)
+    plt.legend(fontsize=15)
+
+    plt.savefig(f'{output_path}/{current_timestamp}_rolling_rate_plot_{window}_window.png', bbox_inches='tight')
+
+    plt.show()
+    plt.close()
+# Plot rolling rate end.
+
+if mode == 4:
+    def hist(series, x_label, title, filename, bins=0, isPhase=False, indicate_outliers=True):
+        # Calculate average and median.
+        mean = np.mean(series)
+        median = np.median(series)
+
+        # Plot histogram, and mean and median vertical lines.
+        plt.figure(figsize=(10, 6))
+        if bins:
+            plt.hist(series, bins=bins, alpha=0.7, edgecolor='black')
+        else:
+            plt.hist(series, bins='auto', alpha=0.7, edgecolor='black')
+        plt.axvline(x=mean, color='g', linestyle='--', label=f'Mean = {mean:.2f}')
+        plt.axvline(x=median, color='g', linestyle='--', label=f'Median = {median:.2f}')
+
+        # Plot shaded outlier region.
+        if indicate_outliers:
+            Q1 = series.quantile(0.25)
+            Q3 = series.quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+            plt.axvline(x=lower_bound, color='r', linestyle=':')
+            plt.axvline(x=upper_bound, color='r', linestyle=':')
+
+        plt.xlabel(x_label, fontsize=15)
+        if isPhase:
+            plt.xlim(left=0, right=24)
+        plt.ylabel('Frequency', fontsize=15)
+        plt.title(title, fontsize=20)
+        plt.legend(fontsize=15)
+        plt.savefig(filename, bbox_inches='tight')
+        plt.show()
+        plt.close()
+    # End def.
+
+    hist(df['Count'], data_label, 'Distribution of Muon Counts',
+         f'{output_path}/{current_timestamp}_muon_count_histogram.png')#, bins=200)
+# Plot counts histogram end.
 
 ''' ***************************************** LOG CONFIGS *************************************** '''
 # Log current configs, write to file.
@@ -236,7 +327,8 @@ log_configs = {
     "gaussian smoothing sigma parameter": gaussian_smoothing_sigma,
     "low pass filter cutoff frequency": f'1/{int(1/low_pass_filter)}' if low_pass_filter else 0,
     "wavelet": f'{wavelet}',
-    "number of scales": scales.max()
+    "scale range": [scales.min(), scales.max()],
+    "rolling rate window (number of points)": window
 }
 with open(f"{output_path}/{current_timestamp}_log.txt", "w") as f:
     json.dump(log_configs, f, indent=4)
